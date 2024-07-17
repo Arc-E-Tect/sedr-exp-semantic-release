c66ae45f9c7f33564965c8eea22013a3983347ea
Arc-E-Tect <ieising@hotmail.com>
2024-07-17 22:12:07 +0200

    fix: follow tutorial from YouTube

--- release.config.js
+++ release.config.js
@@ -6,70 +6,15 @@ module.exports = {
   plugins: [
     [
       "@semantic-release/commit-analyzer",
-      {
-        preset: "angular",
-        releaseRules: [
-          {
-            breaking: true,
-            release: "major",
-          },
-          {
-            type: "feat",
-            release: "minor",
-          },
-          {
-            type: "fix",
-            release: "patch",
-          },
-          {
-            type: "docs",
-            scope: "README",
-            release: "patch",
-          },
-          {
-            type: "chore",
-            release: "patch",
-          }
-        ],
-        parserOpts: {
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
-        },
-      },
-    ],
-    [
       "@semantic-release/release-notes-generator",
+      ["@semantic-release/git",
       {
-        /*  
-            use conventionalcommits instead of conventional-changelog-angular (default)
-            to introduce new sections in changelog
-        */
-        preset: "conventionalcommits",
-        presetConfig: {
-          types: [
-            { type: "feat", section: "Features", hidden: false },
-            { type: "fix", section: "Bug Fixes", hidden: false },
-            { type: "docs", section: "Miscellaneous Chores", hidden: false },
-            { type: "chore", section: "Miscellaneous Chores", hidden: false },
-          ],
-        },
-        parserOpts: {
-          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
-        },
-      },
-    ],
-    ["@semantic-release/npm"],
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md",
-      },
-    ],
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-      },
+        "assets": ["CHANGELOG.md"],
+        "message": "chore:(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }],
     ],
     "@semantic-release/github",
   ],
 };
+
+module.exports = config
